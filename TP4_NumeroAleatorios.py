80283a504ffb77d572d569d3c9c6bf13b62956ac
León Schwegler <48569065+leonsch1997@users.noreply.github.com>
2019-05-21 20:44:14 -0300

    Update TP4_NumeroAleatorios.py

--- TP4_NumeroAleatorios.py
+++ TP4_NumeroAleatorios.py
@@ -1,39 +1,23 @@
 
-# def cuadrado_medios(Z): #Esta funcion tiene que converger a 0
-#    zf=[]
-#    z1s=[]
-#    z1=Z**2                         #zf y z1s son arreglos ... z1 es variable
-#    long=len(str(z1))               #acá lo casteo para sacarle la longitud al numero z1
-#    for j in str(z1):
-#        z1s+=[int(j)]               #lleno el arreglo z1s, cada elemento 1 digito
-#    if (long<=8):
-#        for i in range (8-long):    #si tiene menos de 8 digitos se ponen 0's adelante para llegar a 8
-#            z1s+=[0]
-#    zf=z1s[2:6]                     #al arreglo z1s le corto los 4 del medio y los pongo en zf (recordar que sigue siendo una lista)
-#    s= ''.join(str(e) for e in zf)  #uno todo con un join para convertir la list en string
-#    sf=int(s)  #por ultimo convierto el string nuevamente a int
-#    return sf
-#
-# z=(cuadrado_medios(1009))
-# print(z/10000)
-# for i in range (200):
-#    zi=cuadrado_medios(z)
-#    print(i," - ",float(zi)/10000)
-#    z=zi
-
-
 def cuadradoMedio (z1):
-  z2=z1**2
+    lis=[]
+    for i in range (100):
+        z2=z1**2
+        numero=str(z2)
+        long=len(numero)
+        if(long == 8):
+            z3 = z2[2:6]
+            dev=(int(z3)/10000)
+            z1=int(z3)
+            lis += [dev]
+        else:
+            cerosASumar= (8-long)
+            z0='0'*cerosASumar
+            z3=z0+numero
+            z4=z3[2:6]
+            dev=(int(z4)/10000)
+            z1=int(z4)
+            lis+=[dev]
+    return lis
 
-  long=len(str(z2))
-  if(long == 8):
-     for j in range (30):
-        z3 = z2[2:6]
-        print (z3/10000)
-  else:
-     cerosASumar= (8-long)
-     print(cerosASumar)
-
-  return (cerosASumar)
-
-cuadradoMedio(z1=1009)
+print(cuadradoMedio(1009))
